9933f8f5acf1d66c29b2d4b5ec997bbe96b27e57
Manish Kripalani <67455631+manishkrip@users.noreply.github.com>
2022-06-28 12:54:53 +0530

    Add files via upload

---
+++ Banking_Project/Banking_Project/src/main/java/com/bank_project/Banking_Project/BankingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bank_project.Banking_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingProjectApplication.class, args);
+	}
+
+}
